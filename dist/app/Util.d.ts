53392bf88ff7b3bfa324cfd57504e39fb661e4a4
Maksim Dzeviantseichyk <maksim.dzeviantseichyk@sap.com>
2023-09-12 23:14:25 +0200

    SDECCT-42: builded project

--- dist/app/Util.d.ts
+++ dist/app/Util.d.ts
@@ -1,8 +1,9 @@
 import { Datacenter } from '../@types/Datacenter';
 import { BandwithPerSecond } from '../@types/Bandwidth';
+import { LatencyDataPoint, BandwidthDataPoint } from '../@types/Shared';
 export declare class Util {
-    static getAverageLatency(data: number[] | undefined): number;
-    static getAverageBandwidth(data: BandwithPerSecond[] | undefined): BandwithPerSecond;
+    static getAverageLatency(data: LatencyDataPoint[] | undefined): number;
+    static getAverageBandwidth(data: BandwidthDataPoint[] | undefined): BandwithPerSecond;
     static sortDatacenters(datacenters: Datacenter[]): Datacenter[];
     static getTop3(datacenters: Datacenter[]): Datacenter[];
     static sleep(ms: number): Promise<void>;
